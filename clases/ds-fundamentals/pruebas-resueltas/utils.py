01b670d3896a5d9f954bb001ff67b4c650cd0437
iair <i.linker8@gmail.com>
2024-11-02 07:36:40 -0300

    pauta hotel booking terminada

---
+++ clases/ds-fundamentals/pruebas-resueltas/utils.py
@@ -0,0 +1,38 @@
+import pandas as pd
+import numpy as np
+
+def calculate_na_statistics(df: pd.DataFrame) -> pd.DataFrame:
+    """
+    Calculate the number of non-missing values, missing values, and the percentage of missing values
+    for each column in a DataFrame, and return them as a sorted DataFrame.
+
+    Parameters:
+    ----------
+    df : pd.DataFrame
+        The DataFrame for which to calculate NA statistics.
+
+    Returns:
+    -------
+    pd.DataFrame
+        A DataFrame with columns representing:
+        - 'datos sin NAs en q': Number of non-missing values for each column
+        - 'Na en q': Number of missing values for each column
+        - 'Na en %': Percentage of missing values for each column, sorted in descending order.
+    """
+    qsna = df.shape[0] - df.isnull().sum(axis=0)
+    qna = df.isnull().sum(axis=0)
+    ppna = np.round(100 * (df.isnull().sum(axis=0) / df.shape[0]), 2)
+    aux = {'datos sin NAs en q': qsna, 'Na en q': qna, 'Na en %': ppna}
+    na = pd.DataFrame(data=aux)
+    return na.sort_values(by='Na en %', ascending=False)
+
+# Function to detect outliers using IQR
+def detect_outliers_iqr(data):
+    Q1 = data.quantile(0.25)
+    Q3 = data.quantile(0.75)
+    IQR = Q3 - Q1
+    # Define bounds
+    lower_bound = Q1 - 1.5 * IQR
+    upper_bound = Q3 + 1.5 * IQR
+    # Return True for outliers
+    return (data < lower_bound) | (data > upper_bound)
